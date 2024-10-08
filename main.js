84aef306e4eaacbb1a03f011048cc1e86ff2ede1
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-10-08 14:31:05 +0200

    add: click effect

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
-console.log("Funzioni?");
+//console.log("Funzioni?");
 
 /* 
 Inseriamo in pagina l’immagine della lampadina spenta che trovate in allegato e accanto un bottone con la scritta “Accendi”.
 Al click del bottone, la lampadina dovrà accendersi (dovremo quindi utilizzare l’immagine della lampadina accesa, sempre in allegato)
 */
+const imgEl = document.getElementById("lightbulb-off");
+const secondImgEl = document.getElementById("lightbulb-on");
+const buttonEl = document.querySelector(".button");
+
+buttonEl.addEventListener("click", () => {
+    imgEl.classList.add("new-pick");
+    secondImgEl.classList.remove("new-pick");
+})
