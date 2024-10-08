6a4e7b61f87738304da31b322bba750252d1af77
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-10-08 14:07:29 +0200

    add: image and button

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 console.log("Funzioni?");
+
+/* 
+Inseriamo in pagina l’immagine della lampadina spenta che trovate in allegato e accanto un bottone con la scritta “Accendi”.
+Al click del bottone, la lampadina dovrà accendersi (dovremo quindi utilizzare l’immagine della lampadina accesa, sempre in allegato)
+*/
